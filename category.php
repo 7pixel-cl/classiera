8507b30d3659a5a8d624bb3d5bf9088fb11e9ee5
Marco Alvarado <marco.alvarado@jove.com>
2023-03-01 13:20:56 -0300

    move i inside a

--- category.php
+++ category.php
@@ -508,9 +508,10 @@ if (isset($tag_extra_fields[$cat_id])) {
 									echo '<a href="'.esc_url($homeAdImglink1).'" target="_blank"><img class="img-responsive" alt="image" src="'.esc_url($homeAdImg1).'" /></a>';
 								}
 								?>
-								<a href="https://api.whatsapp.com/send?phone=56953535153&text=Hola+estoy+interesado+en+publicitar+en+Necesito+en+Villarrica" target="_blank">Anuncia con nosotros
+								<a href="https://api.whatsapp.com/send?phone=56953535153&text=Hola,+estoy+interesado+en+publicitar+en+Necesito+en+Villarrica" target="_blank">Anuncia con nosotros
+									<i class="fab fa-whatsapp fa-li"></i> 
 								</a>
-								<i class="fab fa-whatsapp fa-li"></i> 
+								
 							</div>
 						</div>
 					</div>	
